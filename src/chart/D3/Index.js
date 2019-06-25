65e2a0df68826880c16916d86bb5a19f09df4510
DeuklyoungKo <deutschgon@gmail.com>
2019-06-25 15:27:24 +0200

    D3 - Let’s Make a Bar Chart, Parts III is finished (https://github.com/d3/d3/wiki/Tutorials)

--- src/chart/D3/Index.js
+++ src/chart/D3/Index.js
@@ -13,67 +13,81 @@ export default class Index extends Component {
 
     init() {
 
-        var width = 420,
-            barHeight = 20;
+        var margin = {top: 20, right: 30, bottom: 30, left: 40},
+            width = 960 - margin.left - margin.right,
+            height = 500 - margin.top - margin.bottom;
 
-        var x = d3.scaleLinear()
-            .range([0, width]);
+
+        var x = d3.scaleBand()
+            .domain(["A", "B", "C", "D", "E", "F"])
+            .rangeRound([0, width])
+            .padding(0.1);
+
+        var y = d3.scaleLinear()
+            .range([height, 0]);
+
+        var xAxis = d3.axisBottom(x);
+
+        var yAxis = d3.axisLeft(y);
 
         var chart = d3.select(".chart")
-            .attr("width", width);
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-
-        // d3.csv("./data.csv", type, function(error, data) {
+        // d3.csv("data.tsv", type, function(error, data) {
         d3.csv("../data/data.csv").then(function (data) {
-
-            data.forEach(function(d) {
+            data.forEach(function (d) {
                 d.value = +d.value; // coerce to number
                 return d;
             });
+            x.domain(data.map(function (d) {
+                return d.name;
+            }));
+            y.domain([0, d3.max(data, function (d) {
+                return d.value;
+            })]);
 
-            console.log(data);
+            chart.append("g")
+                .attr("class", "x axis")
+                .attr("transform", "translate(0," + height + ")")
+                .call(xAxis);
 
-            x.domain([0, d3.max(data, function(d) { return d.value; })]);
+            chart.append("g")
+                .attr("class", "y axis")
+                .call(yAxis);
 
-            chart.attr("height", barHeight * data.length);
-
-            var bar = chart.selectAll("g")
+            chart.selectAll(".bar")
                 .data(data)
-                .enter().append("g")
-                .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
+                .enter().append("rect")
+                .attr("class", "bar")
+                .attr("x", function (d) {
+                    return x(d.name);
+                })
+                .attr("y", function (d) {
+                    return y(d.value);
+                })
+                .attr("height", function (d) {
+                    return height - y(d.value);
+                })
+                .attr("width", x);
 
-            bar.append("rect")
-                .attr("width", function(d) { return x(d.value); })
-                .attr("height", barHeight - 1);
-
-            bar.append("text")
-                .attr("x", function(d) { return x(d.value) - 3; })
-                .attr("y", barHeight / 2)
-                .attr("dy", ".35em")
-                .text(function(d) { return d.value; });
-        });
-
-        function type(d) {
-            d.value = +d.value; // coerce to number
-            return d;
-        }
-
+        })
     }
 
-        componentDidMount()
-        {
-            this.init();
-        }
-
-
-        render()
-        {
-
-            return (
-                <div>
-                    <svg className="chart"></svg>
-                </div>
-            )
-
-        }
+    componentDidMount() {
+        this.init();
     }
+
+
+    render() {
+
+        return (
+            <div>
+                <svg className="chart"></svg>
+            </div>
+        )
+
+    }
+}
